7ee3b744bc13c46761c87f4648096c8fc9666c2e
尹孟佳 <3495314473@qq.com>
2024-09-02 22:20:29 +0800

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports =  {
+    root: true,
+    "env":{
+        "browser":true,
+        "es2021":true
+    },
+    "extends":[
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:react-hooks/recommended",
+        "plugin:prettier/recommended",
+    ],
+    "overrides":[],
+    "parser":"@typescript-eslint/parser",
+    "parserOptions":{
+        "ecmaVersion":"latest",
+        "sourceType":"module"
+    },
+    "plugins":["react","@typescript-eslint"],
+}
