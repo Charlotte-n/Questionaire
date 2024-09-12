d32ecee390080d48a6e5ac650cc182dd4e77cf8b
尹孟佳 <3495314473@qq.com>
2024-09-12 08:01:41 +0800

    fix: 更新上传测试用例

--- src/components/Uploader/__test__/Uploader.test.tsx
+++ src/components/Uploader/__test__/Uploader.test.tsx
@@ -1,4 +1,10 @@
-import { render, waitFor, fireEvent, cleanup } from '@testing-library/react'
+import {
+    render,
+    waitFor,
+    fireEvent,
+    cleanup,
+    renderHook,
+} from '@testing-library/react'
 import '@testing-library/jest-dom'
 import Uploader from '../index'
 import axios from 'axios'
@@ -8,28 +14,28 @@ jest.mock('axios')
 const mockAxios = axios as jest.Mocked<typeof axios>
 
 describe('Uploader component', () => {
-    let wrapper: HTMLElement
-    let renderer: any
-    beforeAll(() => {
-        const { container, rerender } = render(
-            <Uploader action="mmmm"></Uploader>,
-        )
-        wrapper = container
-        renderer = rerender
-    })
-
     test('renders without crashing', () => {
+        const { container: wrapper } = render(
+            <Uploader action="url"></Uploader>,
+        )
         expect(wrapper.querySelector('div')).toBeInTheDocument()
     })
     //测试ui
     test('basic layout before upload', () => {
+        const { container: wrapper } = render(
+            <Uploader action="url"></Uploader>,
+        )
         expect(wrapper.querySelector('button')).toBeInTheDocument()
         expect(wrapper.querySelector('button span')?.textContent).toBe(
             '上传文件',
         )
         expect(wrapper.querySelector('input')).toBeInTheDocument()
     })
-    test.only('upload process should work fine', async () => {
+    test('upload process should work fine', async () => {
+        const { container: wrapper } = render(
+            <Uploader action="url"></Uploader>,
+        )
+
         mockAxios.post.mockResolvedValueOnce({ fileUploadStatus: 'success' })
         const fileInput = wrapper.querySelector('input') as HTMLInputElement
         const files = [file]
@@ -49,7 +55,7 @@ describe('Uploader component', () => {
             )
         })
     })
-    test.only('upload process should work error', async () => {
+    test('upload process should work error', async () => {
         const { container: wrapper } = render(
             <Uploader action="url"></Uploader>,
         )
