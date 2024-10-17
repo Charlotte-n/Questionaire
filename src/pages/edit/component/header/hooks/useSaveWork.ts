73f68e609b22dd8bb8af90718df532b5fa6fd0a1
yinmengjia <yinmengjia@meituan.com>
2024-10-17 13:59:58 +0800

    feat: 整合和提取保存的功能

--- src/pages/edit/component/header/hooks/useSaveWork.ts
+++ src/pages/edit/component/header/hooks/useSaveWork.ts
@@ -1,2 +1,50 @@
+import React, { useEffect } from 'react'
+import { Modal } from 'antd'
+import { saveTemplateAsync, setIsDirty } from '../../../../../stores/editor'
+import { useNavigate, useParams, useBlocker } from 'react-router-dom'
+import { useAppDispatch, useAppSelector } from '../../../../../stores'
+export const useSaveWork = () => {
+    const dispatch = useAppDispatch()
+    const navigate = useNavigate()
+    const { id } = useParams<{ id: string }>()
+    const { isDirty } = useAppSelector((state) => state.editorSlice)
+    const blocker = useBlocker(!isDirty)
 
+    const saveWorkApi = () => {
+        dispatch(saveTemplateAsync(id as string))
+        dispatch(setIsDirty(false))
+    }
+    //定时保存
+    useEffect(() => {
+        const timer = setInterval(() => {
+            if (isDirty) {
+                saveWorkApi()
+            }
+        }, 1000)
+        return () => {
+            clearInterval(timer)
+        }
+    }, [])
 
+    useEffect(() => {
+        // 当离开此页面的时候，如果没有保存就弹窗
+        if (blocker.state === 'blocked') {
+            Modal.confirm({
+                title: '确认离开',
+                content: '您有未保存的更改，是否确认离开？',
+                okText: '确定',
+                cancelText: '取消',
+                onOk: () => {
+                    blocker.proceed()
+                },
+                onCancel: () => {
+                    blocker.reset()
+                },
+            })
+        }
+    }, [navigate, blocker])
+
+    return {
+        saveWorkApi,
+    }
+}
--- src/pages/edit/component/header/index.tsx
+++ src/pages/edit/component/header/index.tsx
@@ -1,19 +1,18 @@
-import React, { FC, useEffect } from 'react'
-import { Button, Dropdown, MenuProps, message, Modal } from 'antd'
-import { useParams, useNavigate, useBlocker } from 'react-router-dom'
+import React, { FC } from 'react'
+import { Button, Dropdown, MenuProps, message } from 'antd'
+import { useNavigate } from 'react-router-dom'
 import { useAppDispatch, useAppSelector } from '../../../../stores'
-import { saveTemplateAsync, setIsDirty } from '../../../../stores/editor'
+
 import { menuList } from '../../../layout/header/config'
 import { loginout } from '../../../../stores/user'
+import { useSaveWork } from './hooks/useSaveWork'
 
 const EditHeader: FC = () => {
-    const { id } = useParams<{ id: string }>()
     const dispatch = useAppDispatch()
     const navigate = useNavigate()
-    const { isDirty } = useAppSelector((state) => state.editorSlice)
+    const { saveWorkApi } = useSaveWork()
     const { opName } = useAppSelector((state) => state.globalSlice)
     const buttonClassName = 'rounded-full mr-[25px]'
-    const blocker = useBlocker(!isDirty)
 
     const onMenuClick: MenuProps['onClick'] = (e) => {
         if (e.key === '2') {
@@ -24,41 +23,6 @@ const EditHeader: FC = () => {
             }, 1000)
         }
     }
-
-    const saveWorkApi = () => {
-        dispatch(saveTemplateAsync(id as string))
-        dispatch(setIsDirty(false))
-    }
-    //router
-
-    //定时保存
-    useEffect(() => {
-        const timer = setInterval(() => {
-            if (isDirty) {
-                saveWorkApi()
-            }
-        }, 1000)
-        return () => {
-            clearInterval(timer)
-        }
-    }, [])
-
-    useEffect(() => {
-        console.log(blocker.state, isDirty)
-        // 当离开此页面的时候，如果没有保存就弹窗
-        if (blocker.state === 'blocked') {
-            Modal.confirm({
-                title: '确认离开',
-                content: '您有未保存的更改，是否确认离开？',
-                onOk: () => {
-                    blocker.proceed()
-                },
-                onCancel: () => {
-                    blocker.reset()
-                },
-            })
-        }
-    }, [navigate, blocker])
 
     return (
         <div className="flex justify-between">
