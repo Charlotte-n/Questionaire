1c3222397b6699d282ac707a0c41b9b21f0bde02
yinmengjia <yinmengjia@meituan.com>
2024-10-31 15:34:26 +0800

    feat: 提交

--- src/services/config.ts
+++ src/services/config.ts
@@ -1,9 +1,9 @@
 let BASE_URL = ''
-const TIME_OUT = 2000
+// const TIME_OUT = 50
 if (process.env.NODE_ENV === 'development') {
     BASE_URL = 'http://localhost:7001/api'
 } else if (process.env.NODE_ENV === 'production') {
     BASE_URL = 'https://egg.hk.merikle.top/api'
 }
 
-export { BASE_URL, TIME_OUT }
+export { BASE_URL }
--- src/services/index.ts
+++ src/services/index.ts
@@ -1,9 +1,8 @@
-import { BASE_URL, TIME_OUT } from './config'
+import { BASE_URL } from './config'
 import HYRequest from './request'
 
 const hyRequest = new HYRequest({
     baseURL: BASE_URL,
-    timeout: TIME_OUT,
 })
 
 export default hyRequest
